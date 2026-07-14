b507d69d7e4a1ec396bb16cab4a23ad3b7a04b8d
UICO32 <UICO32@users.noreply.github.com>
2026-07-14 23:41:39 +0800

    chore: remove tracked debug artifacts

--- src/converters/_debug-render.ts
+++
@@ -1,8 +0,0 @@
-import { renderBlocksToHTML } from './renderBlocks'
-
-const html = renderBlocksToHTML(JSON.stringify([
-  { type: 'paragraph', content: [{ type: 'text', text: 'Hello world' }] }
-]))
-
-console.log('=== HTML OUTPUT ===')
-console.log(html)
